4c966768e15892a91dab2e7a63569b661ebfbc02
yelo <zhihuzeye@gmail.com>
2017-12-04 21:51:39 +0800

    feat: add middlewares compatibility with Component

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -12,27 +12,31 @@ export function $route (model) {
         fullPath: isEmpty(options) ? `/${this.route}` : `/${this.route}?${querystring.stringify(options)}`,
       }
       this.$log('Route Middleware', 'Ready')
-    }
+    },
   })
 }
 
 // builtin initial middleware for Tina-Page
+function initial () {
+  // init data (just for triggering ``compute`` in this moment)
+  this.setData()
+  this.$log('Initial Middleware', 'Ready')
+}
 export function $initial (model) {
   return addHooks(model, {
-    onLoad () {
-      // init data (just for triggering ``compute`` in this moment)
-      this.setData()
-      this.$log('Initial Middleware', 'Ready')
-    }
+    attached: initial,
+    onLoad: initial,
   })
 }
 
 // builtin log middleware for Tina-Page
+function log () {
+  this.$log = this.constructor.log.bind(this.constructor)
+  this.$log('Log Middleware', 'Ready')
+}
 export function $log (model) {
   return addHooks(model, {
-    beforeLoad () {
-      this.$log = this.constructor.log.bind(this.constructor)
-      this.$log('Log Middleware', 'Ready')
-    }
+    beforeCreate: log,
+    beforeLoad: log,
   })
 }
