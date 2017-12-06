de9ac53334b8125e54dba9632b21c8a1ce266b71
yelo <zhihuzeye@gmail.com>
2017-12-06 18:28:21 +0800

    build: uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
+import uglify from 'rollup-plugin-uglify'
 import base from './rollup.conf.base.js'
 
 export default Object.assign({}, base, {
   output: {
-    file: 'dist/tina.js',
+    file: 'dist/tina.min.js',
     format: 'umd',
     name: 'tina',
   },
+  plugins: base.plugins.concat([
+    uglify(),
+  ]),
 })
