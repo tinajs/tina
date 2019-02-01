cb29f1d72a20a7db9e6f04a95cda221919065f11
yelo <zhihuzeye@gmail.com>
2019-02-01 17:01:54 +0800

    build: generate libs for different platform

--- rollup.config.base.js
+++ rollup.config.base.js
@@ -2,21 +2,35 @@ import commonjs from 'rollup-plugin-commonjs'
 import nodejs from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import builtins from 'rollup-plugin-node-builtins'
+import replace from 'rollup-plugin-replace'
 
-export default {
-  input: 'src/index.js',
-  output: {
-    exports: 'default',
-  },
-  plugins: [
-    builtins(),
-    babel({
-      exclude: ['node_modules/**'],
-      plugins: [
-        'external-helpers',
-      ],
-    }),
-    nodejs(),
-    commonjs(),
-  ],
+export default function createConfig ({ output, env }) {
+  let replacement = {}
+  for (let key in env) {
+    replacement[`process.env.${key}`] = env[key]
+  }
+
+  return {
+    input: 'src/index.js',
+    output: {
+      exports: 'default',
+      format: 'umd',
+      name: 'tina',
+      file: output,
+    },
+    plugins: [
+      replace({
+        values: replacement,
+      }),
+      builtins(),
+      babel({
+        exclude: ['node_modules/**'],
+        plugins: [
+          'external-helpers',
+        ],
+      }),
+      nodejs(),
+      commonjs(),
+    ],
+  }
 }
